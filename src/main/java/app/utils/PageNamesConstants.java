4384efb27fbae73418ebc14ac3a5249f5a2813b3
limonpudding <martyshenko.sergei@yandex.ru>
2018-09-14 17:48:36 +0500

    Обновлен pom.xml Строка sql запроса для создания таблицы вынесена в константу

--- src/main/java/app/utils/PageNamesConstants.java
+++ src/main/java/app/utils/PageNamesConstants.java
@@ -16,4 +16,58 @@ public class PageNamesConstants {
     public static final String ACCESS_DENIED_PAGE = "/accessDenied";
     public static final String ACCOUNTS_MANAGER_PAGE = "/accountsManager";
     public static final String ROLE_CHANGE_PAGE = "/roleChange";
+
+    public static final String CREATE_DB = "create table USERS\n" +
+            "(\n" +
+            "  USERNAME             NVARCHAR2(40) not null\n" +
+            "    primary key,\n" +
+            "  PASSWORD           NVARCHAR2(100) not null\n" +
+            ");" +
+            "INSERT INTO USERS\n" +
+            "(USERNAME,PASSWORD)\n" +
+            "VALUES  ('admin', '$2a$10$5a6vv3yJZuAbpUSU04vAce2d6MACeDHJeDspyulKzbR2.tAu5W2Tm');\n" +
+            "create table USERROLES\n" +
+            "(\n" +
+            "  ID int auto_increment primary key, \n" +
+            "  USERNAME             NVARCHAR2(40) not null,\n" +
+            "  ROLE           NVARCHAR2(40) not null\n" +
+            "); " +
+            "INSERT INTO USERROLES\n" +
+            "(USERNAME,ROLE)\n" +
+            "VALUES  ('admin', 'ROLE_ADMIN');\n" +
+            "create table BINARYOPERATION\n" +
+            "(\n" +
+            "  ID             NVARCHAR2(40) not null\n" +
+            "    primary key,\n" +
+            "  OPERATIONKIND           NVARCHAR2(40),\n" +
+            "  FIRSTOPERAND   CLOB,\n" +
+            "  SECONDOPERAND CLOB,\n" +
+            "  ANSWER         CLOB,\n" +
+            "  IDSESSION      NVARCHAR2(40),\n" +
+            "  TIME           TIMESTAMP(6)\n" +
+            ");" +
+            "create table SINGLEOPERATION\n" +
+            "(\n" +
+            "  ID           NVARCHAR2(40) not null\n" +
+            "    primary key,\n" +
+            "  OPERATIONKIND         NVARCHAR2(40),\n" +
+            "  FIRSTOPERAND CLOB,\n" +
+            "  ANSWER       CLOB,\n" +
+            "  IDSESSION    NVARCHAR2(40),\n" +
+            "  TIME         TIMESTAMP(6)\n" +
+            ");" +
+            "create table CONSTANTS\n" +
+            "(\n" +
+            "  KEY            NVARCHAR2(40) default NULL not null\n" +
+            "    primary key,\n" +
+            "  VALUE  CLOB" +
+            ");" +
+            "create table SESSIONS\n" +
+            "(\n" +
+            "  ID        NVARCHAR2(40) default NULL not null\n" +
+            "    primary key,\n" +
+            "  IP        NVARCHAR2(25),\n" +
+            "  TIMESTART TIMESTAMP,\n" +
+            "  TIMEEND   TIMESTAMP\n" +
+            ");";
 }
--- src/test/java/testconfig/TestConfig.java
+++ src/test/java/testconfig/TestConfig.java
@@ -19,10 +19,14 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 import javax.sql.DataSource;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+
+import static app.utils.PageNamesConstants.CREATE_DB;
 
 @EnableTransactionManagement
 @Configuration
@@ -71,61 +75,7 @@ public class TestConfig implements WebMvcConfigurer {
         try {
             connection = dataSource.getConnection();
             Statement statement = connection.createStatement();
-            statement.executeUpdate(
-                    "create table USERS\n" +
-                            "(\n" +
-                            "  USERNAME             NVARCHAR2(40) not null\n" +
-                            "    primary key,\n" +
-                            "  PASSWORD           NVARCHAR2(100) not null\n" +
-                            ");" +
-                            "INSERT INTO USERS\n" +
-                            "(USERNAME,PASSWORD)\n" +
-                            "VALUES  ('admin', '$2a$10$5a6vv3yJZuAbpUSU04vAce2d6MACeDHJeDspyulKzbR2.tAu5W2Tm');\n" +
-                            "create table USERROLES\n" +
-                            "(\n" +
-                            "  ID int auto_increment primary key, \n" +
-                            "  USERNAME             NVARCHAR2(40) not null,\n" +
-                            "  ROLE           NVARCHAR2(40) not null\n" +
-                            "); " +
-                            "INSERT INTO USERROLES\n" +
-                            "(USERNAME,ROLE)\n" +
-                            "VALUES  ('admin', 'ROLE_ADMIN');\n" +
-                            "create table BINARYOPERATION\n" +
-                            "(\n" +
-                            "  ID             NVARCHAR2(40) not null\n" +
-                            "    primary key,\n" +
-                            "  OPERATIONKIND           NVARCHAR2(40),\n" +
-                            "  FIRSTOPERAND   CLOB,\n" +
-                            "  SECONDOPERAND CLOB,\n" +
-                            "  ANSWER         CLOB,\n" +
-                            "  IDSESSION      NVARCHAR2(40),\n" +
-                            "  TIME           TIMESTAMP(6)\n" +
-                            ");" +
-                            "create table SINGLEOPERATION\n" +
-                            "(\n" +
-                            "  ID           NVARCHAR2(40) not null\n" +
-                            "    primary key,\n" +
-                            "  OPERATIONKIND         NVARCHAR2(40),\n" +
-                            "  FIRSTOPERAND CLOB,\n" +
-                            "  ANSWER       CLOB,\n" +
-                            "  IDSESSION    NVARCHAR2(40),\n" +
-                            "  TIME         TIMESTAMP(6)\n" +
-                            ");" +
-                            "create table CONSTANTS\n" +
-                            "(\n" +
-                            "  KEY            NVARCHAR2(40) default NULL not null\n" +
-                            "    primary key,\n" +
-                            "  VALUE  CLOB" +
-                            ");" +
-                            "create table SESSIONS\n" +
-                            "(\n" +
-                            "  ID        NVARCHAR2(40) default NULL not null\n" +
-                            "    primary key,\n" +
-                            "  IP        NVARCHAR2(25),\n" +
-                            "  TIMESTART TIMESTAMP,\n" +
-                            "  TIMEEND   TIMESTAMP\n" +
-                            ");"
-            );
+            statement.executeUpdate(CREATE_DB);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
